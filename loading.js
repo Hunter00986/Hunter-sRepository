9e7832664e08f30b098e66c41b737414c282598f
Hunter00986 <HunterDunn023@gmail.com>
2026-03-06 04:39:57 +0000

    Thu, Mar 5, 2026, 8:39 PM -08:00

--- loading.js
+++ loading.js
@@ -7,6 +7,7 @@ int = setInterval(blurring, 30)
 
 function blurring() {
     load = load + 1
+    loadText.innerText = `${load}%`;
     loadText.style.opacity = 1 - load / 100;
     bg.style.filter = `blur(${50 - load / 2}px)`;
 
