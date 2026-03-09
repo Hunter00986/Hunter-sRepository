a9d53de96ff87b152968636cc65e8c0649cc66e4
Hunter00986 <HunterDunn023@gmail.com>
2026-03-09 17:10:19 +0000

    Mon, Mar 9, 2026, 10:10 AM -07:00

--- event.js
+++ event.js
@@ -1,6 +1,8 @@
 clickBox = document.querySelector('.click')
 spinBox = document.querySelector('.spin')
 translateBox = document.querySelector('.keydown');
+hoverBox = document.querySelector('.hover')
+dblclickBox = document.querySelector('dbclick')
 
 colors = ['blue', 'green', 'purple', 'red']
 index = 0
@@ -39,3 +41,18 @@ document.addEventListener('keydown', (event) => {
     translateBox.style.bottom = `${positionY}px`;
     translateBox.style.left = `${positionX}px`;
 });
+
+hoverBox.addEventListener('mouseenter', () => {
+    hoverBox.style.height = "200px"
+    hoverBox.style.width = "500px"
+})
+
+hoverBox.addEventListener('mouseleave', () => {
+    hoverBox.style.height = "100px"
+    hoverBox.style.width = "250px"
+})
+
+
+dblclickBox.addEventListener('dblclick', () => {
+ dblclickBox.classList.toggle('fade')
+})
