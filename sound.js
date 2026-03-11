fa49f535af54be72c0ab73a13c5b8884b89fdaaf
Hunter00986 <HunterDunn023@gmail.com>
2026-03-11 16:41:59 +0000

    Wed, Mar 11, 2026, 9:41 AM -07:00

--- sound.js
+++ sound.js
@@ -1,12 +1,20 @@
 buttons = document.querySelector('.buttons')
 applause = document.querySelector('.applause')
+boo = document.querySelector('.boo')
+gasp = document.querySelector('.gasp')
+tada = document.querySelector('.tada')
+victory = document.querySelector('.victory')
+wrong = document.querySelector('.wrong')
 
-btn = document.createElement('button')
-btn.innerText = "applause"
-btn.classList.add('btn')
-btn.addEventListner('click', =>{
-    applause.play()
-})
-buttons.appendChild(btn)
+sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 
+for(let i = 0; i < 6; i = i + 1) {
+    btn = document.createElement('button')
+    btn.innerText = sounds[i]
+    btn.classList.add('btn')
+    btn.addEventListener('click', () =>{
+        sound[i].play()
+    })
+    buttons.appendChild(btn)
 
+}
