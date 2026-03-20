b2bbd37a96d9351e756325e2f7e48d325cafeb32
Hunter00986 <HunterDunn023@gmail.com>
2026-03-20 16:52:15 +0000

    Fri, Mar 20, 2026, 9:52 AM -07:00

--- insect.js
+++ insect.js
@@ -24,7 +24,7 @@ function startGame() {
 function creatInsect() {
     insect = document.createElement('div')
     insect.classList.add('insect')
-    {x,y} = getRandomLocation()
+    const {x,y} = getRandomLocation()
     insect.style.top = `${y}px`
     insect.style.left = `${x}px`
     insect.innerHTML = `<img src="${src}" alt="${alt}" style="transform: rotate(${Math.random()*360}deg)">`
